62ebd3cf5eb2642c6d94dd87d34ad6be525d2b0e
Angela Zhou <angelazhou922@gmail.com>
2018-10-21 19:39:00 -0700

    added comments

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+//when we open the database, we check once to see if it's working. however, we repeatedly check if there is an error. If there is an error, we log it to the console.
 var db = mongoose.connection;
 db.once('open', () => {
   console.log('connnection has been made');
@@ -11,19 +12,18 @@ let RepoSchema = mongoose.Schema({
   repos: Array, 
 });
 
-//creating a model so that whenever we make a new instance of a github, we add it in the repos collection (model) based off of RepoSchema.
+//creating a model so that whenever we make a new instance of a github schema, we add it in the repos collection (model).
 let UsersRepoModel = mongoose.model('UsersReposModel', RepoSchema);
 
 //using validateBeforeSave. This function will save the relevant data from the GitHub API in the mongo database.
 let save = (name, repos, callback) => {
-  console.log(name, repos)
   var repo = new UsersRepoModel({name, repos});
   repo.save((err, repo) => { 
     callback(err, repo);
   });
 }
 
-//find the repo by the name in the model
+//find the repo by the name in the model. We will use this function later in the server to see if the username is already in the database. If so, we will take it out and no need to fetch the info from the api and put it back in the database.
 let findByName = (username, callback) => {
   UsersRepoModel.find({name: username}, (err, repos) => {
     callback(err, repos); 
